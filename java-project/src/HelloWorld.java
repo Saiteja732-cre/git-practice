723f42fd38d3a604ecf11209213642ae79b2984f
Saiteja732-cre <saitejavjl023@gmail.com>
2025-12-25 16:51:37 +0530

    Update HelloWorld.java

--- java-project/src/HelloWorld.java
+++ java-project/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
         System.out.println("This is my first Java project integrated with Git and Jenkins.");
 
         // Print Welcome and Thank you 3 times
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Welcome");
             System.out.println("Thank you");
         }
