5d287ebb37a00a7d2c270ba58ffb1d0ab2f0c0e5
Saiteja732-cre <saitejavjl023@gmail.com>
2025-12-25 16:39:07 +0530

    Update

--- java-project/src/HelloWorld.java
+++ java-project/src/HelloWorld.java
@@ -3,5 +3,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World from Jenkins CI!");
         System.out.println("This is my first Java project integrated with Git and Jenkins.");
+
+        // Print Welcome and Thank you 3 times
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Welcome");
+            System.out.println("Thank you");
+        }
     }
 }
