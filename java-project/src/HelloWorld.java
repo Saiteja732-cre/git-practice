3e14ffd0e489abcfb41ffda50909ce7eac07f66c
root <root@ip-172-31-68-148.ec2.internal>
2025-12-25 07:28:07 +0000

    java

---
+++ java-project/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Jenkins CI!");
+        System.out.println("This is my first Java project integrated with Git and Jenkins.");
+    }
+}
